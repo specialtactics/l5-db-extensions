7d364a88f474d69fc06826df3b78ec72e822bcc4
max.snow <max.snow@4mation.com.au>
2016-07-28 13:59:05 +1000

    using correct builders

--- src/Database/MySqlConnection.php
+++ src/Database/MySqlConnection.php
@@ -2,10 +2,25 @@
 
 namespace SCM\L5DB\Database;
 
+use SCM\L5DB\Database\Schema\MySqlBuilder;
 use SCM\L5DB\Database\Schema\Grammars\MySqlGrammar as SchemaGrammar;
 
 class MySqlConnection extends \Illuminate\Database\MySqlConnection
 {
+    /**
+     * Get a schema builder instance for the connection.
+     *
+     * @return \Illuminate\Database\Schema\MySqlBuilder
+     */
+    public function getSchemaBuilder()
+    {
+        if (is_null($this->schemaGrammar)) {
+            $this->useDefaultSchemaGrammar();
+        }
+
+        return new MySqlBuilder($this);
+    }
+
     /**
      * Get the default schema grammar instance.
      *
--- src/Database/PostgresConnection.php
+++ src/Database/PostgresConnection.php
@@ -3,9 +3,24 @@
 namespace SCM\L5DB\Database;
 
 use SCM\L5DB\Database\Schema\Grammars\PostgresGrammar as SchemaGrammar;
+use SCM\L5DB\Database\Schema\PostgresBuilder;
 
 class PostgresConnection extends \Illuminate\Database\PostgresConnection
 {
+    /**
+     * Get a schema builder instance for the connection.
+     *
+     * @return \Illuminate\Database\Schema\MySqlBuilder
+     */
+    public function getSchemaBuilder()
+    {
+        if (is_null($this->schemaGrammar)) {
+            $this->useDefaultSchemaGrammar();
+        }
+
+        return new PostgresBuilder($this);
+    }
+
     /**
      * Get the default schema grammar instance.
      *
